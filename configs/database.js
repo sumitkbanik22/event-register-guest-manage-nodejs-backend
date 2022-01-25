9aa52ac616cb07991cec9c18c6ca94da4a2289d7
Sumit Banik <sumit@apenning.xyz>
2022-01-26 01:38:40 +0530

    fixed .env variables issue

--- configs/database.js
+++ configs/database.js
@@ -1,10 +1,12 @@
-const env = require('dotenv').config().parsed;
+const dotenv = require('dotenv');
 const mongoose = require("mongoose");
 
-const username = env.DB_USERNAME;
-const password = env.DB_PASSWORD;
-const cluster = env.DB_CLUSTER;
-const dbname = env.DB_NAME;
+dotenv.config();
+
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
+const cluster = process.env.DB_CLUSTER;
+const dbname = process.env.DB_NAME;
 
 mongoose.connect(
   `mongodb+srv://${username}:${password}@${cluster}.mongodb.net/${dbname}?retryWrites=true&w=majority`, 
